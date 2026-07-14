7244b3015dc369cb20335466df1c83bf34407ccb
karmuhil pandiyan <karmuhilpandiyan17@gmail.com>
2026-07-14 14:16:10 +0530

    Create 2556-ConvertTheTemperature

---
+++ 2556-ConvertTheTemperature/2556-ConvertTheTemperature.java
@@ -0,0 +1,10 @@
+// Last updated: 14/07/2026, 14:16:08
+class Solution {
+    public double[] convertTemperature(double c) {
+        double k=c+273.15;
+        double f=c*1.80+32.00;
+        double[] result={k,f};
+        return result;
+     
+    }
+}
