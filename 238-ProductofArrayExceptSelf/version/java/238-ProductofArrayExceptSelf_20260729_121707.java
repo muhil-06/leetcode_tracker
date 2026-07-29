fb9f8849e6a4961ccc7e8fc1de2587e15cb1b197
karmuhil pandiyan <karmuhilpandiyan17@gmail.com>
2026-07-29 12:17:08 +0530

    Create 238-ProductofArrayExceptSelf

---
+++ 238-ProductofArrayExceptSelf/version/java/238-ProductofArrayExceptSelf_20260729_121707.java
@@ -0,0 +1,14 @@
+// Last updated: 29/07/2026, 12:17:07
+1class Solution {
+2    public int minMoves(int[] num) {
+3        int min=num[0];
+4        int sum=0;
+5        for(int i=0;i<num.length;i++){
+6            min=Math.min(min,num[i]);
+7        }
+8        for(int i=0;i<num.length;i++){
+9            sum+=num[i]-min;
+10        }
+11        return sum;
+12    }
+13}
