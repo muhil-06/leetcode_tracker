67079deaa263808476956f67e32d866305eb5a64
karmuhil pandiyan <karmuhilpandiyan17@gmail.com>
2026-09-21 15:15:06 +0530

    Create 3925-ConcatenateArrayWithReverse

---
+++ 3925-ConcatenateArrayWithReverse/3925-ConcatenateArrayWithReverse.java
@@ -0,0 +1,15 @@
+// Last updated: 21/09/2026, 15:15:05
+class Solution {
+    public int[] concatWithReverse(int[] num) {
+        int[] res=new int[num.length*2];
+        for(int i=0;i<num.length;i++){
+            res[i]=num[i];
+        }
+        int j=num.length;
+        for(int i=num.length-1;i>=0;i--){
+            res[j]=num[i];
+            j++;
+        }
+        return res;
+    }
+}
