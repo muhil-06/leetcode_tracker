6082f38e5d7139f1e5da9dc625efcebf9b217943
karmuhil pandiyan <karmuhilpandiyan17@gmail.com>
2026-09-21 15:18:16 +0530

    Create 904-FruitIntoBaskets

---
+++ 904-FruitIntoBaskets/904-FruitIntoBaskets.py
@@ -0,0 +1,16 @@
+# Last updated: 21/09/2026, 15:18:15
+class Solution(object):
+    def totalFruit(self, fruits):
+        count={}
+        left=0
+        ans=0
+        for right in range(len(fruits)):
+            count[fruits[right]]=count.get(fruits[right],0)+1
+            while len(count)>2:
+                count[fruits[left]]-=1
+                if count[fruits[left]]==0:
+                   del count[fruits[left]]
+                left+=1
+            ans=max(ans,right-left+1)
+        return ans                        
+        
