b102f4bbb3e7118f88d16b10c8c2906820281730
karmuhil pandiyan <karmuhilpandiyan17@gmail.com>
2026-09-21 15:17:43 +0530

    Create 1025-DivisorGame

---
+++ 1025-DivisorGame/1025-DivisorGame.java
@@ -0,0 +1,11 @@
+// Last updated: 21/09/2026, 15:17:42
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n==1)
+        return false;
+        if(n%2==0)
+        return true;
+        else
+        return false;
+    }
+}
