fcc19bd7061ff308f58a1d93194c35b6c2610cd8
karmuhil pandiyan <karmuhilpandiyan17@gmail.com>
2026-09-21 15:16:02 +0530

    Create 2119-ANumberAfterADoubleReversal

---
+++ 2119-ANumberAfterADoubleReversal/2119-ANumberAfterADoubleReversal.java
@@ -0,0 +1,8 @@
+// Last updated: 21/09/2026, 15:16:01
+class Solution {
+    public boolean isSameAfterReversals(int n) {
+        if(n==0)
+        return true ;
+        return!(n%10==0);
+    }
+}
