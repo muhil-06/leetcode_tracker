58729b34d3fe48e6ac7bcd5d99a4dde993d2410a
karmuhil pandiyan <karmuhilpandiyan17@gmail.com>
2026-09-21 15:15:38 +0530

    Create 2469-ConvertTheTemperature

---
+++ 2469-ConvertTheTemperature/2469-ConvertTheTemperature.java
@@ -0,0 +1,10 @@
+// Last updated: 21/09/2026, 15:15:37
+class Solution {
+    public double[] convertTemperature(double c) {
+        double k=c+273.15;
+        double f=c*1.80+32.00;
+        double[] result={k,f};
+        return result;
+     
+    }
+}
