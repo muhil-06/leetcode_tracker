907481b83b1c7342f75b3909e5fdbf5e69c0fe65
karmuhil pandiyan <karmuhilpandiyan17@gmail.com>
2026-09-21 15:15:10 +0530

    Create 2769-FindTheMaximumAchievableNumber

---
+++ 2769-FindTheMaximumAchievableNumber/2769-FindTheMaximumAchievableNumber.java
@@ -0,0 +1,9 @@
+// Last updated: 21/09/2026, 15:15:09
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        t=t*2;
+        int n=num+t;
+        return n;
+        
+    }
+}
